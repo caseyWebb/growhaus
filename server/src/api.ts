f7bc40d0661f1899477550d130272cc17a4c92a6
Casey Webb <notcaseywebb@gmail.com>
2019-07-25 20:00:39 -0500

    test

--- server/src/api.ts
+++ server/src/api.ts
@@ -4,9 +4,10 @@ import { weather } from './weather'
 
 export const websocketRoutes = [
   route.all('/agent/:name', (ctx, name: string) => {
-    const sendBrightness = () => ctx.websocket.send(weather.current.brightness)
+    // const sendBrightness = () => ctx.websocket.send(weather.current.brightness)
+    const sendBrightness = () => ctx.websocket.send(10)
     sendBrightness()
-    weather.subscribe(sendBrightness)
+    // weather.subscribe(sendBrightness)
   }),
 
   route.all('/web', (ctx) => {
