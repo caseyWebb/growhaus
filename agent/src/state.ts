ce4fd0d44d1b4ad492fe095be96f61e8a6e833af
Casey Webb <notcaseywebb@gmail.com>
2019-08-05 18:01:53 -0500

    fix(agent): Allow zero-point usage of state.setBrightness

--- agent/src/state.ts
+++ agent/src/state.ts
@@ -1,8 +1,10 @@
+import autobind from 'autobind-decorator'
 import { Subscribable } from '@caseywebb/growhaus'
 
 export class AgentState extends Subscribable {
   public readonly brightness: number = 100
 
+  @autobind
   public setBrightness(brightness: number) {
     Object.assign(this, { brightness })
     this.next()
